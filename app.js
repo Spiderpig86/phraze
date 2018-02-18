398b141dc4665e6ddd4aa3a71b05f65682df338c
spiderpig86 <slim679975@gmail.com>
2018-02-17 20:04:44 -0500

    chore(app.js): Re-added routing for login page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var index = require('./routes/index');
+var login = require('./routes/login');
 
 // Grab the credentials to access the database.
 var credentials = require('./keys');
@@ -29,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-//app.use('/users', users);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
