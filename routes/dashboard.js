722257c0489855331741104a68eead2c440aff83
cwong2015 <sirhc8002@gmail.com>
2018-02-18 05:01:23 -0500

    made dashboard redirect to login page when user is not logged in

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,7 +4,11 @@ var users = require('../models/users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if(req.user)
   res.render('dashboard', { title: 'Phraze', user: req.user });
+  else{
+    res.redirect('login');
+  }
 
   var user_phrases = [];
   users.find({}, function(err, data) {
