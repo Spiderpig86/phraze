7b2c1370559d1c0d94a2664a92bace48f089b56c
cwong2015 <sirhc8002@gmail.com>
2018-02-18 03:56:00 -0500

    login and logout button functionality work and switch accordingly

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Phraze' });
+  res.render('login', { title: 'Phraze', user: req.user });
 });
 
 module.exports = router;
