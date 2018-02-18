ab232ccac0e3a195adb34faa8b27444e30ad3d09
Fanny <fannydai@gmail.com>
2018-02-17 22:37:29 -0500

    I didn't change anything but git wants me to commit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// app.use('/', index);
 app.use('/login', login);
 
 // catch 404 and forward to error handler
